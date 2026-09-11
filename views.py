76f5f67caa7fb9854e3263643c86650e1bd82499
Igor <igor.martins.senai@gmail.com>
2026-09-11 10:11:45 -0300

    adiciona suporte para upload de imagens na criação de jogos

--- views.py
+++ views.py
@@ -30,8 +30,13 @@ def criar():
     db.session.add(novo_jogo)
     db.session.commit()
 
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    arquivo.save(f'{upload_path}/capa{novo_jogo.id}.jpg')
 
     return redirect(url_for('index'))
+
+
 
 
 @app.route('/editar/<int:id>')
