8dfa6f17557fd4b84eff198f3180d23b52db5e8a
Igor <igor.martins.senai@gmail.com>
2026-09-12 19:10:20 -0300

    Atualiza configuração

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
 import os
+from dotenv import load_dotenv
 
-SECRET_KEY = 'ruass'
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 SQLALCHEMY_DATABASE_URI = \
-    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
-        SGBD = 'mysql+mysqlconnector',
-        usuario = 'igor',
-        senha = '123456',
-        servidor = 'localhost',
-        database = 'jogoteca'
+    'mysql+mysqlconnector://{usuario}:{senha}@{servidor}/{database}'.format(
+        usuario=os.getenv('DB_USER'),
+        senha=os.getenv('DB_PASSWORD'),
+        servidor=os.getenv('DB_HOST'),
+        database=os.getenv('DB_NAME')
     )
 
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
