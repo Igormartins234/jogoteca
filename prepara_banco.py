35ce7018e8fde1edc016da5bde34f1cad8099bae
Igor <igor.martins.senai@gmail.com>
2026-09-12 19:03:32 -0300

    Add Flask-Bcrypt extension for bcrypt hashing and comparison
    - Introduced `flask_bcrypt.py` providing a simple interface for password hashing and checking using bcrypt.
    - Added helper functions `generate_password_hash` and `check_password_hash` for ease of use.
    - Implemented `Bcrypt` class to initialize with Flask app and manage configuration options.
    - Included support for handling long passwords and customizable bcrypt parameters.
    - Created `py.typed` file for type hinting support in the bcrypt package.

--- prepara_banco.py
+++ prepara_banco.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from mysql.connector import errorcode
+from flask_bcrypt import generate_password_hash
 
 print("Conectando...")
 try:
@@ -58,9 +59,9 @@ for tabela_nome in TABLES:
 # inserindo usuarios
 usuario_sql = 'INSERT INTO usuarios (nome, nickname, senha) VALUES (%s, %s, %s)'
 usuarios = [
-      ("Bruno Divino", "BD", "alohomora"),
-      ("Camila Ferreira", "Mila", "paozinho"),
-      ("Guilherme Louro", "Cake", "python_eh_vida")
+      ("João", "joao", generate_password_hash("321").decode('utf-8')),
+      ("Lucas", "lucas", generate_password_hash("123").decode('utf-8')),
+      ("Maria", "maria", generate_password_hash("abc").decode('utf-8'))
 ]
 cursor.executemany(usuario_sql, usuarios)
 
