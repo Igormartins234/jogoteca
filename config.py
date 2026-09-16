27fa539967304eb133ba587c33abae760fb441b0
Igor <igor.martins.senai@gmail.com>
2026-09-15 22:56:39 -0300

    migra banco para PostgreSQL Neon

--- config.py
+++ config.py
@@ -1,19 +1,10 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy.engine import URL
 
 load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-SQLALCHEMY_DATABASE_URI = URL.create(
-    drivername='mysql+mysqlconnector',
-    username=os.getenv('DB_USER'),
-    password=os.getenv('DB_PASSWORD'),
-    host=os.getenv('DB_HOST'),
-    port=int(os.getenv('DB_PORT')),
-    database=os.getenv('DB_NAME')
-)
-
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
