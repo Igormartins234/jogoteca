60024fb5aef287fb2e2804b4a2644848748d2d64
Igor <igor.martins.senai@gmail.com>
2026-09-15 23:03:33 -0300

    corrige configuracao PostgreSQL Neon

--- config.py
+++ config.py
@@ -5,6 +5,13 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = \
+    'postgresql+psycopg2://{usuario}:{senha}@{servidor}:{porta}/{database}'.format(
+        usuario=os.getenv('DB_USER'),
+        senha=os.getenv('DB_PASSWORD'),
+        servidor=os.getenv('DB_HOST'),
+        porta=os.getenv('DB_PORT'),
+        database=os.getenv('DB_NAME')
+    )
 
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
