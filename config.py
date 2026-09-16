5c7708477847b051125f1cd9adfb03286d41536c
Igor <igor.martins.senai@gmail.com>
2026-09-15 22:09:45 -0300

    corrige conexão com banco no Render

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
 import os
 from dotenv import load_dotenv
+from sqlalchemy.engine import URL
 
 load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-SQLALCHEMY_DATABASE_URI = \
-    'mysql+mysqlconnector://{usuario}:{senha}@{servidor}:{porta}/{database}'.format(
-        usuario=os.getenv('DB_USER'),
-        senha=os.getenv('DB_PASSWORD'),
-        servidor=os.getenv('DB_HOST'),
-        porta=os.getenv('DB_PORT'),
-        database=os.getenv('DB_NAME')
-    )
+SQLALCHEMY_DATABASE_URI = URL.create(
+    drivername='mysql+mysqlconnector',
+    username=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    host=os.getenv('DB_HOST'),
+    port=int(os.getenv('DB_PORT')),
+    database=os.getenv('DB_NAME')
+)
 
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
